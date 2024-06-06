48b2cc19dc5c114efef68974ae408b67575a0871
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2024-06-06 14:24:31 +0700

    Add link to go to user page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-[80vh] items-center justify-center p-24 ${inter.className}`}
     >
-      Hello
+      <Link href='/users'>
+        <Button className='bg-gradient-to-r from-blue-400 via-cyan-500 to-indigo-600 text-white font-bold py-2 px-4 rounded hover:from-blue-500 hover:via-cyan-600 hover:to-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
+          Go to User page
+        </Button>
+      </Link>
     </main>
   );
 }
